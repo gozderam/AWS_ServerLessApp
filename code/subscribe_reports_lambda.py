deca6fd43bff4a90af442260e6e100e98cabd613
gozderam <gozdera.michal@gmail.com>
2022-07-06 00:22:28 +0200

    Added code

---
+++ code/subscribe_reports_lambda.py
@@ -0,0 +1,58 @@
+import json
+import boto3
+import os
+
+def handler(event, context):
+
+    # get allowed origin 
+    cf_client = boto3.client('cloudformation')
+    stack_outputs = cf_client.describe_stacks(StackName='BucketsStack')['Stacks'][0]['Outputs']
+    frontend_host = next(out for out in stack_outputs if out['OutputKey']=='WebsiteURL')['OutputValue']
+    
+    try:
+        protocol = 'email'
+
+        # enivioroment ans ssm variables
+        FUNCTION_REGION = os.environ['awsRegion']
+
+        ssmClient = boto3.client('ssm', region_name=FUNCTION_REGION)
+        topic_arn = ssmClient.get_parameter(Name='/params/reportTopicArn')['Parameter']['Value'] 
+
+        # create topic from arn
+        sns = boto3.resource('sns')    
+        topic = sns.Topic(topic_arn)
+
+        # get emailaddress from body
+        email_address =  json.loads(event['body'])['emailAddress']
+        
+        # adding subscribtion
+        try:
+            subscription = topic.subscribe(
+                Protocol=protocol, Endpoint=email_address, ReturnSubscriptionArn=True)
+            print(f"Subscribed {protocol} {email_address}, to topic {topic.arn}.")
+        except Exception as e:
+            print(f"Couldn't subscribe {protocol} {email_address} to topic {topic.arn}.")
+            raise            
+                
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'accept,accept-encoding,accept-language,access-control-request-method,connection,host,origin,sec-fetch-dest,sec-fetch-mode,sec-fetch-site,user-agent,content-type',
+                'Access-Control-Allow-Origin': frontend_host,
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE'
+            },
+            'body': json.dumps('Successful subscription')
+        }
+
+    except  Exception as err:
+        
+        print(err)
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Headers': 'accept,accept-encoding,accept-language,access-control-request-method,connection,host,origin,sec-fetch-dest,sec-fetch-mode,sec-fetch-site,user-agent,content-type',
+                'Access-Control-Allow-Origin': frontend_host,
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,DELETE'
+            },
+            'body': "Internal server error encountered"
+        }
